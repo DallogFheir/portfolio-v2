169cd661035eb19373d8becb54c051a17d14f82d
DallogFheir <dallog.fheir@gmail.com>
2024-08-05 19:37:12 +0200

    feat: SAP UI5 & Anki Duplicards

--- src/components/sections/technologiesIWorkWith.ts
+++ src/components/sections/technologiesIWorkWith.ts
@@ -1,34 +1,39 @@
 import type { SectionContent } from "../../types/Section";
+import Express from "./assets/technologies/express.png";
+import JavaScript from "./assets/technologies/javascript.png";
+import Python from "./assets/technologies/python.png";
+import React from "./assets/technologies/react.png";
+import SapUi5 from "./assets/technologies/sapui5.png";
+import Sass from "./assets/technologies/sass.png";
+import TypeScript from "./assets/technologies/typescript.png";
 
 export default [
   {
     title: "JavaScript",
-    imgSrc:
-      "https://user-images.githubusercontent.com/25181517/117447155-6a868a00-af3d-11eb-9cfe-245df15c9f3f.png",
+    imgSrc: JavaScript,
   },
   {
     title: "TypeScript",
-    imgSrc:
-      "https://user-images.githubusercontent.com/25181517/183890598-19a0ac2d-e88a-4005-a8df-1ee36782fde1.png",
+    imgSrc: TypeScript,
   },
   {
     title: "React",
-    imgSrc:
-      "https://user-images.githubusercontent.com/25181517/183897015-94a058a6-b86e-4e42-a37f-bf92061753e5.png",
+    imgSrc: React,
   },
   {
     title: "Express",
-    imgSrc:
-      "https://user-images.githubusercontent.com/25181517/183859966-a3462d8d-1bc7-4880-b353-e2cbed900ed6.png",
+    imgSrc: Express,
   },
   {
     title: "Sass",
-    imgSrc:
-      "https://user-images.githubusercontent.com/25181517/192158956-48192682-23d5-4bfc-9dfb-6511ade346bc.png",
+    imgSrc: Sass,
   },
   {
     title: "Python",
-    imgSrc:
-      "https://user-images.githubusercontent.com/25181517/183423507-c056a6f9-1ba8-4312-a350-19bcbc5a8697.png",
+    imgSrc: Python,
+  },
+  {
+    title: "SAP UI5",
+    imgSrc: SapUi5,
   },
 ] satisfies SectionContent[];
